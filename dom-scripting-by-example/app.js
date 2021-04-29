804f928c304bfe208a538a9eed56e7292a4d46b4
kdevnel <22053773+kdevnel@users.noreply.github.com>
2021-04-29 12:47:20 +0200

    Lesson - Removing Names

--- dom-scripting-by-example/app.js
+++ dom-scripting-by-example/app.js
@@ -1,11 +1,8 @@
 const form = document.getElementById('registrar');
 const input = form.querySelector('input');
+const invitedList = document.getElementById('invitedList');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const text = input.value;
-  input.value = '';
-  const invitedList = document.getElementById('invitedList');
+function createLI(text) {
   const li = document.createElement('li');
   li.textContent = text;
   const label = document.createElement('label');
@@ -14,5 +11,37 @@ form.addEventListener('submit', (e) => {
   checkbox.type = 'checkbox';
   label.appendChild(checkbox);
   li.appendChild(label);
+  const button = document.createElement('button');
+  button.textContent = 'Remove';
+  li.appendChild(button);
+  return li;
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const text = input.value;
+  input.value = '';
+  const li = createLI(text);
   invitedList.appendChild(li);
+});
+
+invitedList.addEventListener('change', (e) => {
+  const checkbox = e.target;
+  const checked = checkbox.checked;
+  const listItem = checkbox.parentNode.parentNode;
+
+  if (checked) {
+    listItem.className = 'responded';
+  } else {
+    listItem.className = '';
+  }
+});
+
+invitedList.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    const li = e.target.parentNode;
+    const ul = li.parentNode;
+
+    ul.removeChild(li);
+  }
 });
