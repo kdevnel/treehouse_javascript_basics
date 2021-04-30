bb881c619628b3b20d00259d9aefebf827f248b8
kdevnel <22053773+kdevnel@users.noreply.github.com>
2021-04-30 09:46:12 +0200

    Lesson - Refactor 2: Readable branching logic

--- dom-scripting-by-example/app.js
+++ dom-scripting-by-example/app.js
@@ -1,35 +1,34 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("registrar");
+  const mainDiv = document.querySelector(".main");
+  const input = form.querySelector("input");
+  const invitedList = document.getElementById("invitedList");
 
-  const form = document.getElementById('registrar');
-  const mainDiv = document.querySelector('.main');
-  const input = form.querySelector('input');
-  const invitedList = document.getElementById('invitedList');
-
-  const div = document.createElement('div');
-  const filterLabel = document.createElement('label');
-  const filterCheckBox = document.createElement('input');
+  const div = document.createElement("div");
+  const filterLabel = document.createElement("label");
+  const filterCheckBox = document.createElement("input");
 
   filterLabel.textContent = "Hide those who haven't responded";
-  filterCheckBox.type = 'checkbox';
+  filterCheckBox.type = "checkbox";
   div.appendChild(filterLabel);
   div.appendChild(filterCheckBox);
   mainDiv.insertBefore(div, invitedList);
-  filterCheckBox.addEventListener('change', (e) => {
+  filterCheckBox.addEventListener("change", (e) => {
     const isChecked = e.target.checked;
     const lis = invitedList.children;
     if (isChecked) {
       for (let i = 0; i < lis.length; i++) {
         let li = lis[i];
-        if (li.className === 'responded') {
-          li.style.display = '';
+        if (li.className === "responded") {
+          li.style.display = "";
         } else {
-          li.style.display = 'none';
+          li.style.display = "none";
         }
       }
     } else {
       for (let i = 0; i < lis.length; i++) {
         let li = lis[i];
-        li.style.display = '';
+        li.style.display = "";
       }
     }
   });
@@ -47,60 +46,68 @@ document.addEventListener('DOMContentLoaded', () => {
       return element;
     }
 
-    const li = document.createElement('li');
-    appendToLI('span', 'textContent', text);
-    appendToLI('label', 'textContent', 'Confirmed')
-      .appendChild(createElement('input', 'type', 'checkbox'));
-    appendToLI('button', 'textContent', 'edit');
-    appendToLI('button', 'textContent', 'remove');
+    const li = document.createElement("li");
+    appendToLI("span", "textContent", text);
+    appendToLI("label", "textContent", "Confirmed").appendChild(
+      createElement("input", "type", "checkbox")
+    );
+    appendToLI("button", "textContent", "edit");
+    appendToLI("button", "textContent", "remove");
     return li;
   }
 
-  form.addEventListener('submit', (e) => {
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
     const text = input.value;
-    input.value = '';
+    input.value = "";
     const li = createLI(text);
     invitedList.appendChild(li);
   });
 
-  invitedList.addEventListener('change', (e) => {
+  invitedList.addEventListener("change", (e) => {
     const checkbox = e.target;
     const checked = checkbox.checked;
     const listItem = checkbox.parentNode.parentNode;
 
     if (checked) {
-      listItem.className = 'responded';
+      listItem.className = "responded";
     } else {
-      listItem.className = '';
+      listItem.className = "";
     }
   });
 
-  invitedList.addEventListener('click', (e) => {
-    if (e.target.tagName === 'BUTTON') {
+  invitedList.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
       const button = e.target;
       const li = button.parentNode;
       const ul = li.parentNode;
+      const action = button.textContent;
+      // we add this object to make branching logic more readable
+      const nameActions = {
+        remove: () => {
+          ul.removeChild(li);
+        },
+        edit: () => {
+          const span = li.firstElementChild;
+          const input = document.createElement("input");
+          input.type = "text";
+          input.value = span.textContent;
+          li.insertBefore(input, span);
+          li.removeChild(span);
+          button.textContent = "save";
+        },
+        save: () => {
+          const input = li.firstElementChild;
+          const span = document.createElement("span");
+          span.textContent = input.value;
+          li.insertBefore(span, input);
+          li.removeChild(input);
+          button.textContent = "edit";
+        },
+      };
 
-      if (button.textContent === 'remove') {
-        ul.removeChild(li);
-      } else if (button.textContent === 'edit') {
-        const span = li.firstElementChild;
-        const input = document.createElement('input');
-        input.type = 'text';
-        input.value = span.textContent;
-        li.insertBefore(input, span);
-        li.removeChild(span);
-        button.textContent = 'save';
-      } else if (button.textContent === 'save') {
-        const input = li.firstElementChild;
-        const span = document.createElement('span');
-        span.textContent = input.value;
-        li.insertBefore(span, input);
-        li.removeChild(input);
-        button.textContent = 'edit';
-      }
+      // select and run action in button's name
+      nameActions[action]();
     }
   });
-
 });
