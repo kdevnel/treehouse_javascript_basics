0a0efc27fec15860a572eeb8f8a1300dff808d8f
kdevnel <22053773+kdevnel@users.noreply.github.com>
2021-04-30 08:35:29 +0200

    Lesson - Filter Invitees Who Have Not Responded

--- dom-scripting-by-example/app.js
+++ dom-scripting-by-example/app.js
@@ -1,10 +1,43 @@
 const form = document.getElementById('registrar');
+const mainDiv = document.querySelector('.main');
 const input = form.querySelector('input');
 const invitedList = document.getElementById('invitedList');
 
+const div = document.createElement('div');
+const filterLabel = document.createElement('label');
+const filterCheckBox = document.createElement('input');
+
+filterLabel.textContent = "Hide those who haven't responded";
+filterCheckBox.type = 'checkbox';
+div.appendChild(filterLabel);
+div.appendChild(filterCheckBox);
+mainDiv.insertBefore(div, invitedList);
+filterCheckBox.addEventListener('change', (e) => {
+  const isChecked = e.target.checked;
+  const lis = invitedList.children;
+  if (isChecked) {
+    for (let i = 0; i < lis.length; i++) {
+      let li = lis[i];
+      if (li.className === 'responded') {
+        li.style.display = '';
+      } else {
+        li.style.display = 'none';
+      }
+    }
+  } else {
+    for (let i = 0; i < lis.length; i++) {
+      let li = lis[i];
+      li.style.display = '';
+    }
+  }
+});
+
+
 function createLI(text) {
   const li = document.createElement('li');
-  li.textContent = text;
+  const span = document.createElement('span');
+  span.textContent = text;
+  li.appendChild(span);
   const label = document.createElement('label');
   label.textContent = 'Confirmed';
   const checkbox = document.createElement('input');
@@ -13,11 +46,11 @@ function createLI(text) {
   li.appendChild(label);
 
   const editButton = document.createElement('button');
-  editButton.textContent = 'Edit';
+  editButton.textContent = 'edit';
   li.appendChild(editButton);
 
   const removeButton = document.createElement('button');
-  removeButton.textContent = 'Remove';
+  removeButton.textContent = 'remove';
   li.appendChild(removeButton);
   return li;
 }
@@ -48,10 +81,23 @@ invitedList.addEventListener('click', (e) => {
     const li = button.parentNode;
     const ul = li.parentNode;
 
-    if (button.textContent === 'Remove') {
+    if (button.textContent === 'remove') {
       ul.removeChild(li);
-    } else if (button.textContent === 'Edit') {
-      console.log('edit');
+    } else if (button.textContent === 'edit') {
+      const span = li.firstElementChild;
+      const input = document.createElement('input');
+      input.type = 'text';
+      input.value = span.textContent;
+      li.insertBefore(input, span);
+      li.removeChild(span);
+      button.textContent = 'save';
+    } else if (button.textContent === 'save') {
+      const input = li.firstElementChild;
+      const span = document.createElement('span');
+      span.textContent = input.value;
+      li.insertBefore(span, input);
+      li.removeChild(input);
+      button.textContent = 'edit';
     }
   }
 });
