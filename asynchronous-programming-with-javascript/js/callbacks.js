95bd592a6223a087bad5a60ae48ff5f19135986c
kdevnel <22053773+kdevnel@users.noreply.github.com>
2021-05-06 11:08:21 +0200

    Practicing callbacks

--- asynchronous-programming-with-javascript/js/callbacks.js
+++ asynchronous-programming-with-javascript/js/callbacks.js
@@ -4,16 +4,23 @@ const peopleList = document.getElementById("people");
 const btn = document.querySelector("button");
 
 // Make an AJAX request
-function getJSON(url) {
+function getJSON(url, callback) {
   const xhr = new XMLHttpRequest();
   xhr.open("GET", url);
   xhr.onload = () => {
     if (xhr.status === 200) {
       let data = JSON.parse(xhr.responseText);
-      console.log(data);
+      //we take the callback function and pass it the data variable
+      return callback(data);
     }
   };
   xhr.send();
+}
+
+function getProfiles(json) {
+  json.people.map((person) => {
+    getJSON(wikiUrl + person.name, generateHTML);
+  });
 }
 
 // Generate the markup for each profile
@@ -28,4 +35,7 @@ function generateHTML(data) {
     `;
 }
 
-btn.addEventListener("click", () => getJSON(astrosUrl));
+btn.addEventListener("click", (event) => {
+  getJSON(astrosUrl, getProfiles);
+  event.target.remove();
+});
