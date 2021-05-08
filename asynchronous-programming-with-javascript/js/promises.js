27d4e82dc576c33ae7138cf0ab3e32642f304147
kdevnel <22053773+kdevnel@users.noreply.github.com>
2021-05-08 10:18:22 +0200

    combining async/await with promises for easier to read code

--- asynchronous-programming-with-javascript/js/promises.js
+++ asynchronous-programming-with-javascript/js/promises.js
@@ -3,28 +3,14 @@ const wikiUrl = "https://en.wikipedia.org/api/rest_v1/page/summary/";
 const peopleList = document.getElementById("people");
 const btn = document.querySelector("button");
 
-// Removing this in favour of the fetch() method which does most of the same task automatically
-// function getJSON(url) {
-//   return new Promise((resolve, reject) => {
-//     const xhr = new XMLHttpRequest();
-//     xhr.open("GET", url);
-//     xhr.onload = () => {
-//       if (xhr.status === 200) {
-//         let data = JSON.parse(xhr.responseText);
-//         resolve(data);
-//       } else {
-//         reject(Error(xhr.statusText));
-//       }
-//     };
-//     xhr.onerror = () => reject(Error("A network error occurred."));
-//     xhr.send();
-//   });
-// }
-
 function getProfiles(json) {
   const profiles = json.people.map((person) => {
+    const craft = person.craft;
     return fetch(wikiUrl + person.name)
       .then((response) => response.json())
+      .then((profile) => {
+        return { ...profile, craft };
+      })
       .catch((err) => console.log("Error Fetching Wiki", err));
   });
   return Promise.all(profiles);
@@ -43,6 +29,7 @@ function generateHTML(data) {
     if (person.type === "standard") {
       section.innerHTML = `
       <img src=${person.thumbnail.source}>
+      <span>${person.craft}</span>
       <h2>${person.title}</h2>
       <p>${person.description}</p>
       <p>${person.extract}</p>
