47daa939f12f6321d4d2ba5e15cacdcbdc024552
kdevnel <22053773+kdevnel@users.noreply.github.com>
2021-05-18 11:03:54 +0200

    Final project

---
+++ OOJS-challenge/My Final Project/js/app.js
@@ -0,0 +1,15 @@
+const game = new Game();
+
+/**
+ * Listens for click on `#begin-game` and calls startGame() on game object
+ */
+document.getElementById('begin-game').addEventListener('click', function(){
+    game.startGame();
+    this.style.display = 'none';
+    document.getElementById('play-area').style.opacity = '1';
+});
+
+/**
+ * Listen for keyboard presses
+ */
+document.addEventListener("keydown", (event) => {game.handleKeydown(event)});
